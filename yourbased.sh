68e17f40516fe5d0f4eb63ddc2f5cb4d003f3716
Keheliya Gallaba <keheliya.gallaba@gmail.com>
2019-08-08 07:35:30 -0400

    Create yourbased.sh

---
+++ yourbased.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -ex
+export DEBIAN_FRONTEND=noninteractive
+apt-get update && apt-get install -y tzdata
+gem install bundler -v 2.0.1
+# install
+bundle install --without development production
+# before_script
+cp config/database.yml.travis config/database.yml
+bundle exec rake db:create db:migrate RAILS_ENV=test
+bundle exec rake admin:categories:create
+# script
+rake
