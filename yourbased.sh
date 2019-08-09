6a6f94747a6e4a8b88fe505789c9980a677ce289
Keheliya Gallaba <keheliya.gallaba@gmail.com>
2019-08-08 21:27:18 -0400

    Update yourbased.sh

--- yourbased.sh
+++ yourbased.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 export DEBIAN_FRONTEND=noninteractive
-apt-get update && apt-get install -y tzdata libpq-dev
+apt-get update && apt-get install -y tzdata libpq-dev postgresql
 gem install bundler -v 2.0.1
 # install
 bundle install --without development production
